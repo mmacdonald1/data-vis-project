b68fdd4546727ac23bea6136da28491e98155b9d
mmacdonald <melmacd94@gmail.com>
2022-02-07 15:47:10 -0500

    Hello World

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 // port
 const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(port, () => {
   console.log("server started at port: " + port);
 });
